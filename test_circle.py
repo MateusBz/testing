9250b6c80f86b458f72c0cab3264ad3cc27b9099
mateusz <mateusz@gmail.com>
2019-10-20 12:18:28 +0200

    pytest class cd

---
+++ test_circle.py
@@ -0,0 +1,23 @@
+import math
+
+import pytest
+
+import circle
+
+
+def test_area():
+    assert pytest.approx(circle.circle_are(1), 0.001) == math.pi
+    assert pytest.approx(circle.circle_are(2), 0.001) == math.pi*(2 **2)
+    assert pytest.approx(circle.circle_are(2.2), 0.001) == math.pi*(2.2 **2)
+
+
+def test_area_exception():
+    with pytest.raises(ValueError):
+        circle.circle_are(-3)
+
+
+def test_area_type_exeption():
+    with pytest.raises(TypeError):
+        circle.circle_are('asf')
+
+
